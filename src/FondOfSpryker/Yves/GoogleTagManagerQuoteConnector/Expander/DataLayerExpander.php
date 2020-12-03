2b1eec4501c7dad0b2a2014624dc7d49fa107355
Pascal Fischer <p-fischer@gmx.net>
2020-12-03 09:13:33 +0100

    DataLayerExpander: Get QuoteTransfer from $twigVariableBlag instead CartClient. Delete Models, replaced by DataLayerExpander

--- src/FondOfSpryker/Yves/GoogleTagManagerQuoteConnector/Expander/DataLayerExpander.php
+++ src/FondOfSpryker/Yves/GoogleTagManagerQuoteConnector/Expander/DataLayerExpander.php
@@ -2,18 +2,14 @@
 
 namespace FondOfSpryker\Yves\GoogleTagManagerQuoteConnector\Expander;
 
-use Facebook\WebDriver\Exception\NullPointerException;
 use FondOfSpryker\Shared\GoogleTagManagerQuoteConnector\GoogleTagManagerQuoteConnectorConstants as ModuleConstants;
 use FondOfSpryker\Yves\GoogleTagManagerQuoteConnector\Dependency\GoogleTagManagerQuoteConnectorToCartClientInterface;
+use Generated\Shared\Transfer\QuoteTransfer;
 use Generated\Shared\Transfer\TotalsTransfer;
-use Spryker\Shared\Kernel\Transfer\Exception\NullValueException;
-use Spryker\Shared\Log\LoggerTrait;
 use Spryker\Shared\Money\Dependency\Plugin\MoneyPluginInterface;
 
 class DataLayerExpander implements DataLayerExpanderInterface
 {
-    use LoggerTrait;
-
     /**
      * @var \Spryker\Shared\Money\Dependency\Plugin\MoneyPluginInterface
      */
@@ -25,11 +21,6 @@ class DataLayerExpander implements DataLayerExpanderInterface
     protected $cartClient;
 
     /**
-     * @var \Generated\Shared\Transfer\QuoteTransfer
-     */
-    protected $quoteTransfer;
-
-    /**
      * @param \FondOfSpryker\Yves\GoogleTagManagerQuoteConnector\Dependency\GoogleTagManagerQuoteConnectorToCartClientInterface $cartClient
      */
     public function __construct(
@@ -39,7 +30,6 @@ class DataLayerExpander implements DataLayerExpanderInterface
     {
         $this->moneyPlugin = $moneyPlugin;
         $this->cartClient = $cartClient;
-        $this->quoteTransfer = $this->cartClient->getQuote();
     }
 
     /**
@@ -51,42 +41,50 @@ class DataLayerExpander implements DataLayerExpanderInterface
      */
     public function expand(string $page, array $twigVariableBag, array $dataLayer): array
     {
+        $quoteTransfer = $twigVariableBag[ModuleConstants::PARAMETER_QUOTE];
+
         $dataLayer[ModuleConstants::FIELD_TRANSACTION_ENTITY] = 'QUOTE';
-        $dataLayer[ModuleConstants::FIELD_CUSTOMER_EMAIL] = $this->getCustomerEmail();
-        $dataLayer[ModuleConstants::FIELD_EXTERNAL_ID_HASH] = sha1($this->getCustomerEmail());
-        $dataLayer[ModuleConstants::FIELD_TRANSACTION_AFFILIATION] = $this->quoteTransfer->getStore()->getName();
-        $dataLayer[ModuleConstants::FIELD_TRANSACTION_PRODUCTS_SKUS] = $this->getProductSkus();
-        $dataLayer[ModuleConstants::FIELD_TRANSACTION_TAX] = $this->getTax();
-        $dataLayer[ModuleConstants::FIELD_TRANSACTION_TOTAL] = $this->getTotal();
-        $dataLayer[ModuleConstants::FIELD_TRANSACTION_WITHOUT_SHIPPING_AMOUNT] = $this->getTotalWithoutShippingAmount();
+        $dataLayer[ModuleConstants::FIELD_CUSTOMER_EMAIL] = $this->getCustomerEmail($quoteTransfer);
+        $dataLayer[ModuleConstants::FIELD_EXTERNAL_ID_HASH] = $this->getCustomerEmail($quoteTransfer)
+            ? sha1($this->getCustomerEmail($quoteTransfer))
+            : null;
+        $dataLayer[ModuleConstants::FIELD_TRANSACTION_AFFILIATION] = $quoteTransfer->getStore()->getName();
+        $dataLayer[ModuleConstants::FIELD_TRANSACTION_PRODUCTS_SKUS] = $this->getProductSkus($quoteTransfer);
+        $dataLayer[ModuleConstants::FIELD_TRANSACTION_TAX] = $this->getTax($quoteTransfer);
+        $dataLayer[ModuleConstants::FIELD_TRANSACTION_TOTAL] = $this->getTotal($quoteTransfer);
+        $dataLayer[ModuleConstants::FIELD_TRANSACTION_WITHOUT_SHIPPING_AMOUNT] = $this->getTotalWithoutShippingAmount($quoteTransfer);
 
         return $this->deleteEmptyIndexFromDataLayer($dataLayer);
     }
 
     /**
-     * @return string
+     * @param QuoteTransfer $quoteTransfer
+     *
+     * @return null|string
      */
-    protected function getCustomerEmail(): string
+    protected function getCustomerEmail(QuoteTransfer $quoteTransfer): ?string
     {
-        if ($this->quoteTransfer->getBillingAddress() === null) {
-            return '';
+        if ($quoteTransfer->getBillingAddress() === null) {
+            return null;
         }
 
-        if ($this->quoteTransfer->getBillingAddress()->getEmail() === null) {
-            return '';
+        if ($quoteTransfer->getBillingAddress()->getEmail() === null) {
+            return null;
         }
 
-        return $this->quoteTransfer->getBillingAddress()->getEmail();
+        return $quoteTransfer->getBillingAddress()->getEmail();
     }
 
     /**
+     * @param QuoteTransfer $quoteTransfer
+     *
      * @return array
      */
-    protected function getProductSkus(): array
+    protected function getProductSkus(QuoteTransfer $quoteTransfer): array
     {
         $skuCollection = [];
 
-        foreach ($this->quoteTransfer->getItems() as $itemTransfer) {
+        foreach ($quoteTransfer->getItems() as $itemTransfer) {
             $skuCollection[] = $itemTransfer->getSku();
         }
 
@@ -94,45 +92,49 @@ class DataLayerExpander implements DataLayerExpanderInterface
     }
 
     /**
+     * @param QuoteTransfer $quoteTransfer
+     *
      * @return float
      */
-    protected function getTax(): ?float
+    protected function getTax(QuoteTransfer $quoteTransfer): ?float
     {
-        if ($this->quoteTransfer->getTotals() === null) {
+        if ($quoteTransfer->getTotals() === null) {
             return null;
         }
 
-        if ($this->quoteTransfer->getTotals()->getTaxTotal() === null) {
+        if ($quoteTransfer->getTotals()->getTaxTotal() === null) {
             return null;
         }
 
-        return (float) $this->quoteTransfer->getTotals()->getTaxTotal()->getAmount();
+        return (float) $quoteTransfer->getTotals()->getTaxTotal()->getAmount();
     }
 
     /**
+     * @param QuoteTransfer $quoteTransfer
+     *
      * @return float
      */
-    protected function getTotal(): ?float
+    protected function getTotal(QuoteTransfer $quoteTransfer): ?float
     {
-        if ($this->quoteTransfer->getTotals() === null) {
+        if ($quoteTransfer->getTotals() === null) {
             return null;
         }
 
         return $this->moneyPlugin->convertIntegerToDecimal(
-            (int) $this->quoteTransfer->getTotals()->getGrandTotal()
+            (int) $quoteTransfer->getTotals()->getGrandTotal()
         );
     }
 
     /**
      * @return float|null
      */
-    protected function getTotalWithoutShippingAmount(): ?float
+    protected function getTotalWithoutShippingAmount(QuoteTransfer $quoteTransfer): ?float
     {
-        $shipmentTotal = $this->quoteTransfer->getTotals() instanceof TotalsTransfer
-            ? (int)$this->quoteTransfer->getTotals()->getShipmentTotal() : null;
+        $shipmentTotal = $quoteTransfer->getTotals() instanceof TotalsTransfer
+            ? (int)$quoteTransfer->getTotals()->getShipmentTotal() : null;
 
-        $grandTotal = $this->quoteTransfer->getTotals() instanceof TotalsTransfer
-            ? (int)$this->quoteTransfer->getTotals()->getGrandTotal() : null;
+        $grandTotal = $quoteTransfer->getTotals() instanceof TotalsTransfer
+            ? (int)$quoteTransfer->getTotals()->getGrandTotal() : null;
 
         if ($shipmentTotal === null || $grandTotal === null) {
             return null;
